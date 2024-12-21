b15c2c3e83294a03503bc1d3efced24c6534188f
yexiyue <yexiyue666@qq.com>
2024-12-21 17:58:28 +0800

    feat: update release

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 cargo fmt --all --check
 cargo clippy --all-targets --all-features --workspace -- -D warnings
 cargo release changes
-cargo release version patch --execute
-cargo release commit --execute
-cargo release publish --registry crates-io --execute
-cargo release tag --execute
-cargo release push --execute
+cargo release version patch --execute --no-confirm
+cargo release commit --execute --no-confirm
+cargo release publish --registry crates-io --execute --no-confirm
+cargo release tag --execute --no-confirm
+cargo release push --execute --no-confirm
