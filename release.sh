555066f2324d569f3d6a6ef90ac7cb5a633c2c0e
yexiyue <yexiyue666@qq.com>
2025-01-05 11:56:23 +0800

    chore: Release

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@ cargo fmt --all --check
 cargo clippy --all-targets --all-features --workspace -- -D warnings
 cargo release changes
 cargo release version patch --execute --no-confirm --workspace
-cargo release commit --execute --no-confirm --workspace
+cargo release commit --execute --no-confirm
 cargo release publish --registry crates-io --execute --no-confirm --workspace
 cargo release tag --execute --no-confirm --workspace
 cargo release push --execute --no-confirm --workspace
